de32888a85dc13ffce781aa1a09a2bb3f67ef9f0
Ujjwal <ujjwaljindal835@gmail.com>
2024-05-14 18:46:31 +0530

    created segment why choose us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import HeroSection from "@/components/ui/HeroSection";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
     <FeaturedCourses/>
+    <WhyChooseUs/>
     </main>
   );
 }
