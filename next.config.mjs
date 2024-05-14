17ebc887f155301155169d8ea2f427d353f5fedc
Ujjwal <ujjwaljindal835@gmail.com>
2024-05-14 22:25:53 +0530

    fixed the relative path of images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = (process.env.NODE_ENV = "production");
+
 const nextConfig = {
+  basePath: isProd ? "/music-academy" : "",
   output: "export",
   distDir: "dist",
   images: {
